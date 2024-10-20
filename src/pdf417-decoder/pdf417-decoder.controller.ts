8687e952426c9cd7a8e56074eec9797fd7d1bb57
TamaraBenitez <tamaraelizabethbenitez98@gmail.com>
2024-10-20 18:46:32 -0300

    fix error

--- src/pdf417-decoder/pdf417-decoder.controller.ts
+++ src/pdf417-decoder/pdf417-decoder.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UserService } from 'src/user/user.service';
 import { Pdf417DecoderService } from './pdf417-decoder.service';
@@ -27,40 +27,52 @@ export class Pdf417DecoderController {
       return { valid: false, message: 'No se pudo leer el código.' };
     }
 
-    const data = results.getText().split('@');
-    const tramite = data[0];
-    const apellido = data[1].toUpperCase();
-    const nombre = data[2].split(' ')[0].toUpperCase();
-    const dni = data[4];
-    const fechaNacimiento = data[6];
+    try {
+      const data = results.getText().split('@');
+      const tramite = data[0];
+      const apellido = data[1].split(' ')[0].toUpperCase();
+      const nombre = data[2].split(' ')[0].toUpperCase();
+      console.log('apellido extraido del dni', apellido)
+      const dni = data[4];
+      const fechaNacimiento = data[6];
 
-    const user = await this.userService.findOneById(body.userId);
-    if (!user) {
-      return { valid: false, message: 'Usuario no encontrado.' };
+      const user = await this.userService.findOneById(body.userId);
+      if (!user) {
+        return { valid: false, message: 'Usuario no encontrado.' };
+      }
+      const genderString = data[3];
+      let gender: Gender;
+
+      if (genderString === 'M') {
+        gender = Gender.MALE;
+      } else if (genderString === 'F') {
+        gender = Gender.FEMALE;
+      }
+      const discrepancies = [];
+
+      if (tramite !== user.nroTramiteDni) discrepancies.push('trámite');
+      console.log('apellido del usuario', user.surname.toUpperCase())
+      if (apellido !== user.surname.split(' ')[0].toUpperCase()) discrepancies.push('apellido');
+      if (nombre !== user.name.split(' ')[0].toUpperCase()) discrepancies.push('nombre');
+      if (dni !== user.nroDni) discrepancies.push('DNI');
+      if (gender !== user.gender) discrepancies.push('género');
+
+      const isValid = discrepancies.length === 0;
+
+      if (isValid) {
+        await this.userService.update(body.userId, { ...user, isUserVerified: true });
+      }
+      return { valid: isValid, discrepancies: isValid ? null : discrepancies, results: data };
     }
-    const genderString = data[3];
-    let gender: Gender;
 
-    if (genderString === 'M') {
-      gender = Gender.MALE;
-    } else if (genderString === 'F') {
-      gender = Gender.FEMALE;
+
+    catch (error) {
+      throw new BadRequestException("Este codigo de barras no pertenece a un dni.")
     }
-    const discrepancies = [];
-
-    if (tramite !== user.nroTramiteDni) discrepancies.push('trámite');
-    if (apellido !== user.surname.toUpperCase()) discrepancies.push('apellido');
-    if (nombre !== user.name.split(' ')[0].toUpperCase()) discrepancies.push('nombre');
-    if (dni !== user.nroDni) discrepancies.push('DNI');
-    if (gender !== user.gender) discrepancies.push('género');
-
-    const isValid = discrepancies.length === 0;
-
-    if (isValid) {
-      await this.userService.update(body.userId, { ...user, isUserVerified: true });
-    }
-    return { valid: isValid, discrepancies: isValid ? null : discrepancies, results: data };
-
   }
 
+
+
 }
+
+
--- src/pdf417-decoder/pdf417-decoder.service.ts
+++ src/pdf417-decoder/pdf417-decoder.service.ts
@@ -1,9 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PDF417Reader, BinaryBitmap, HybridBinarizer, RGBLuminanceSource } from '@zxing/library';
 import { decode as decodeJpeg } from 'jpeg-js';
 import { PNG } from 'pngjs';
-import { existsSync } from 'fs';
-import * as sharp from 'sharp';
+
 
 const barcodeScanner = new PDF417Reader();
 
@@ -15,42 +14,7 @@ export class Pdf417DecoderService {
         this.codeReader = new PDF417Reader();
     }
 
-    async decodePdf417FromImage(filePath: string) {
-        // Verifica si el archivo existe
-        if (!existsSync(filePath)) {
-            throw new Error(`El archivo no existe en la ruta: ${filePath}`);
-        }
 
-        console.log('File path to decode:', filePath);
-        console.log('Checking if file exists...');
-
-        try {
-            const { data, info } = await sharp(filePath)
-                .raw()
-                .ensureAlpha()
-                .toBuffer({ resolveWithObject: true });
-
-            console.log('Image data:', data);
-            console.log('Image info:', info);
-
-            // Convierte el buffer a Uint8ClampedArray
-            const clampedArray = new Uint8ClampedArray(data);
-
-            // Crea un objeto RGBLuminanceSource
-            const luminanceSource = new RGBLuminanceSource(clampedArray, info.width, info.height);
-            const bitmap = new BinaryBitmap(new HybridBinarizer(luminanceSource));
-
-            // Intenta decodificar el PDF417
-            const result = this.codeReader.decode(bitmap);
-
-            return {
-                text: result.getText(), // Cambia a result.text si es necesario
-            };
-        } catch (error) {
-            console.error('Error al procesar la imagen:', error);
-            throw new Error('Error al procesar la imagen');
-        }
-    }
 
     detectAndScan(fileData: Buffer, mimeType: string) {
         let rawFileData;
@@ -60,7 +24,7 @@ export class Pdf417DecoderService {
         } else if (mimeType === 'image/png') {
             rawFileData = PNG.sync.read(fileData);
         } else {
-            throw new Error('Unsupported file format');
+            throw new BadRequestException('El formato del archivo no esta soportado.');
         }
 
         try {
