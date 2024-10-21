64c24e7cddeb0e1e615d1df4f62611db81032529
TamaraBenitez <tamaraelizabethbenitez98@gmail.com>
2024-10-20 21:42:40 -0300

    fix errors

--- src/pdf417-decoder/pdf417-decoder.controller.ts
+++ src/pdf417-decoder/pdf417-decoder.controller.ts
@@ -1,74 +1,91 @@
-import { BadRequestException, Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Body, Controller, HttpCode, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UserService } from 'src/user/user.service';
 import { Pdf417DecoderService } from './pdf417-decoder.service';
 import { Gender } from 'src/user/entities/user.entity';
+import { format, parse } from 'date-fns';
 
 @Controller('pdf417-decoder')
 export class Pdf417DecoderController {
 
   constructor(private readonly userService: UserService, private readonly decoderService: Pdf417DecoderService) { }
 
-  @Post('decodeLnc')
-  @UseInterceptors(FileInterceptor('file'))
-  async decodeLnc(@UploadedFile() file: Express.Multer.File) {
-    const results = this.decoderService.detectAndScan(file.buffer, file.mimetype);
-    if (!results) {
-      return { valid: false, message: 'No se pudo leer el código.' };
-    }
-    return { valid: true, results: results.getText() };
-  }
 
   @Post('decode')
   @UseInterceptors(FileInterceptor('file'))
+  @HttpCode(200)
   async decodePdf417(@UploadedFile() file: Express.Multer.File, @Body() body: { userId: string }) {
     const results = this.decoderService.detectAndScan(file.buffer, file.mimetype);
     if (!results) {
-      return { valid: false, message: 'No se pudo leer el código.' };
+      throw new BadRequestException('No se pudo leer el código.');
     }
 
     try {
+
+      if (!results.getText().includes('@')) {
+        throw new BadRequestException('Este código de barras no pertenece a un DNI.');
+      }
       const data = results.getText().split('@');
       const tramite = data[0];
       const apellido = data[1].split(' ')[0].toUpperCase();
       const nombre = data[2].split(' ')[0].toUpperCase();
-      console.log('apellido extraido del dni', apellido)
       const dni = data[4];
-      const fechaNacimiento = data[6];
+      const fechaNacimientoString = data[6]; // "07/10/1994"
+
+
+      // Parseamos la fecha de data[6] que está en formato dd/MM/yyyy
+      const fechaNacimiento = parse(fechaNacimientoString, 'dd/MM/yyyy', new Date());
+
+
+
+      // Formateamos también la fecha de data[6] para comparar solo las fechas
+      const fechaNacimientoFormatted = format(fechaNacimiento, 'yyyy-MM-dd');
 
       const user = await this.userService.findOneById(body.userId);
       if (!user) {
-        return { valid: false, message: 'Usuario no encontrado.' };
+        throw new BadRequestException('Usuario no encontrado.');
       }
       const genderString = data[3];
-      let gender: Gender;
+      const genderMap: { [key: string]: Gender } = {
+        'M': Gender.MALE,
+        'F': Gender.FEMALE,
+      };
 
-      if (genderString === 'M') {
-        gender = Gender.MALE;
-      } else if (genderString === 'F') {
-        gender = Gender.FEMALE;
-      }
-      const discrepancies = [];
+      const gender = genderMap[genderString];
+      const userBirthDate = user.birthDate; // "1998-06-11 00:00:00"
+      // Formateamos la fecha de nacimiento del usuario para obtener solo la parte de la fecha (sin tiempo)
+      const userBirthDateFormatted = format(new Date(userBirthDate), 'yyyy-MM-dd');
 
-      if (tramite !== user.nroTramiteDni) discrepancies.push('trámite');
-      console.log('apellido del usuario', user.surname.toUpperCase())
-      if (apellido !== user.surname.split(' ')[0].toUpperCase()) discrepancies.push('apellido');
-      if (nombre !== user.name.split(' ')[0].toUpperCase()) discrepancies.push('nombre');
-      if (dni !== user.nroDni) discrepancies.push('DNI');
-      if (gender !== user.gender) discrepancies.push('género');
+      const comparisons = [
+        { field: tramite, expected: user.nroTramiteDni, label: 'trámite' },
+        { field: apellido, expected: user.surname.split(' ')[0].toUpperCase(), label: 'apellido' },
+        { field: nombre, expected: user.name.split(' ')[0].toUpperCase(), label: 'nombre' },
+        { field: dni, expected: user.nroDni, label: 'DNI' },
+        { field: gender, expected: user.gender, label: 'género' },
+        { field: fechaNacimientoFormatted, expected: userBirthDateFormatted, label: 'fecha de nacimiento' },
+      ];
+
+      const discrepancies = comparisons
+        .filter(comp => comp.field !== comp.expected)
+        .map(comp => comp.label);
 
       const isValid = discrepancies.length === 0;
 
-      if (isValid) {
-        await this.userService.update(body.userId, { ...user, isUserVerified: true });
+      if (!isValid) {
+        throw new BadRequestException('La información del DNI no coincide con los datos del usuario.');
       }
-      return { valid: isValid, discrepancies: isValid ? null : discrepancies, results: data };
+      await this.userService.update(body.userId, { ...user, isUserVerified: true });
+
+      return { valid: true };
+
     }
-
-
     catch (error) {
-      throw new BadRequestException("Este codigo de barras no pertenece a un dni.")
+      throw new BadRequestException(
+        error.message || 'Ocurrió un error al procesar el código de barras.'
+      );
     }
+
+
   }
 
 
