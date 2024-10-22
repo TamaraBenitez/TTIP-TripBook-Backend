425a35e892d9d13df16b9eba39ea7129745f6fec
TamaraBenitez <tamaraelizabethbenitez98@gmail.com>
2024-10-22 20:00:31 -0300

    edit configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,38 +6,37 @@ import { TripUserModule } from './trip-user/trip-user.module';
 import { AuthModule } from './auth/auth.module';
 import { Pdf417DecoderModule } from './pdf417-decoder/pdf417-decoder.module';
 import { ConfigModule } from '@nestjs/config';
-import configuration from 'config/configuration';
+import configuration from './config/configuration';
 import { FileUploadModule } from './file-upload/file-upload.module';
 import { TripCoordinateModule } from './trip-coordinate/trip-coordinate.module';
-import { CompareImageController } from './compare-image/compare-image.controller';
 import { CompareImageService } from './compare-image/compare-image.service';
 import { CompareImageModule } from './compare-image/compare-image.module';
 
 @Module({
   imports: [
-  TypeOrmModule.forRoot({
-    type: "mysql",
-    host: "localhost",
-    port: 3307,
-    username: "tripbook",
-    password: "root",
-    database: "db_tripbook",
-    autoLoadEntities: true,
-    synchronize: true,
-    logger: 'debug',
-  }), 
-  ConfigModule.forRoot({
-    load: [configuration],
-    isGlobal: true
-  }),
-  UserModule,
-  forwardRef(() => TripModule),
-  forwardRef(() => TripUserModule),
-  TripCoordinateModule, 
-  AuthModule, 
-  Pdf417DecoderModule,
-  FileUploadModule,
-  CompareImageModule],
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3307,
+      username: "tripbook",
+      password: "root",
+      database: "db_tripbook",
+      autoLoadEntities: true,
+      synchronize: true,
+      logger: 'debug',
+    }),
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true
+    }),
+    UserModule,
+    forwardRef(() => TripModule),
+    forwardRef(() => TripUserModule),
+    TripCoordinateModule,
+    AuthModule,
+    Pdf417DecoderModule,
+    FileUploadModule,
+    CompareImageModule],
   controllers: [],
   providers: [CompareImageService],
 })
