1801beeda99ab01bdcf8040a2f87eaad4c6bf8aa
TamaraBenitez <tamaraelizabethbenitez98@gmail.com>
2024-10-25 23:26:13 -0300

    add tests initialize

--- src/compare-image/compare-image.module.ts
+++ src/compare-image/compare-image.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { CompareImageController } from './compare-image.controller';
 import { CompareImageService } from './compare-image.service';
 import { UserModule } from 'src/user/user.module';
 
 
 @Module({
     imports: [UserModule],
-    controllers: [CompareImageController],
     providers: [CompareImageService],
     exports: [CompareImageService]
 })
