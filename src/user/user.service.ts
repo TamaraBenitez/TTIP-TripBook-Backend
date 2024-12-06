f26bf9ac34b328cac7d2fa82c4873fa8998e91ec
TamaraBenitez <tamaraelizabethbenitez98@gmail.com>
2024-12-06 13:05:57 -0300

    edit profile

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,10 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { VerifyUserDto } from './dto/verify-user.dto';
+import { plainToInstance } from 'class-transformer';
+import { UpdateUserResponseDto } from './dto/update-user-response.dto';
 
 
 @Injectable()
@@ -29,9 +33,10 @@ export class UserService {
     });
   }
 
-  async update(id: string, updatedUser): Promise<User> {
+  async update(id: string, updatedUser: UpdateUserDto | VerifyUserDto): Promise<UpdateUserResponseDto> {
     await this.userRepository.update(id, updatedUser);
-    return await this.findOneById(id);
+    const user = await this.findOneById(id);
+    return plainToInstance(UpdateUserResponseDto, user, { excludeExtraneousValues: true });
   }
 
 
